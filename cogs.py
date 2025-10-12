6d136d56b2b2210949ab86db61e3b46145679c0e
Cereal_Dev <ajedavid1@gmail.com>
2025-10-12 18:56:48 +0100

    Added the vars that would help me finish the program successfully

--- cogs.py
+++ cogs.py
@@ -6,7 +6,10 @@ comb = [0,00,0]
 cog1 = 1
 cog2 = 1
 cog3 = 10
+ada = 0 #this person takes the sum of all the 792 different digits
+will = 0 #this person multiplies the three numbers in each of the cogs each turn and adds the sum of of all 792 multiplications 
 print(startcomb)
+n =0
 while comb != startcomb :
     
     if cog1 < cog16:
@@ -19,4 +22,6 @@ while comb != startcomb :
         cog3 +=1
     else: cog3 = startcomb[1]
     comb = [cog1, cog3, cog2]
+    n += 1
     print(comb)
+print(n)
